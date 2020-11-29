2fc5f76c9f6d1a6226913516df7ad05d8ccc3154
Topin <ltopin@gmail.com>
2020-11-29 12:04:05 -0300

    Testes

---
+++ src/fibonacci.js
@@ -0,0 +1,14 @@
+class Fibonacci  {
+    *execute (input, current = 0, next = 1) {
+        if(input === 0) {
+            return 0
+        }
+        //retorna o valor
+        yield current
+        // deleta a funcao, mas nao retorna o valor
+        yield* this.execute(input -1, next, current + next)
+    }
+
+}
+
+module.exports = Fibonacci
---
+++ src/fibonacci.tes.js
@@ -0,0 +1,38 @@
+const Fibonacci = require('./fibonacci')
+const sinon = require('sinon')
+const assert = require('assert')
+
+;(async() => {
+    {
+        const fibonacci = new Fibonacci()
+        const spy = sinon.spy(fibonacci, fibonacci.execute.name)
+        //generators retornam iterators, (.next)
+        //existem 3 formas de ler os dados
+        //usando as funcoes .next, for await e rest/spread
+
+        for await(const i of fibonacci.execute(3)) {
+
+        }
+
+        const expectedCallCount = 4
+        assert.deepStrictEqual(spy.callCount, expectedCallCount)
+
+    }
+
+    {
+        const fibonacci = new Fibonacci()
+        const spy = sinon.spy(fibonacci, fibonacci.execute.name)
+        const [...results ] = fibonacci.execute(5)
+
+        const { args } = spy.getCall(2)
+        const expectedResult = [ 0, 1, 1, 2, 3]
+        const expectedParams = Object.values({
+            input: 3,
+            current: 1,
+            next: 2
+        })
+
+        assert.deepStrictEqual(args, expectedParams)
+        assert.deepStrictEqual(results, expectedResult)
+    }
+})()
